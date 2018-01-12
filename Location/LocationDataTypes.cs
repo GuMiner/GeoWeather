35106cbd301d7c2dfa87cf2a6f44381a9dd2103b
GuMiner <gus.gran@gmail.com>
2018-01-11 19:39:45 -0800

    Cleaning up location querying using the bing maps API.

---
+++ Location/LocationDataTypes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace GeoWeather.Location
+{
+    /// <summary>
+    /// Defines data types used for location deserialziation
+    /// </summary>
+    internal class LocationDataTypes
+    {
+        public class Resources
+        {
+            public List<ResourceSet> ResourceSets { get; set; }
+        }
+
+        public class ResourceSet
+        {
+            public List<Resource> Resources { get; set; }
+        }
+
+        public class Resource
+        {
+            public Point Point { get; set; }
+        }
+
+        public class Point
+        {
+            public List<double> Coordinates { get; set; }
+        }
+    }
+}
--- Location/LocationQuerier.cs
+++ Location/LocationQuerier.cs
@@ -1,57 +1,45 @@
-﻿using BotCommon;
+﻿using GeoWeather.Properties;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Device.Location;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace GeoWeather
+namespace GeoWeather.Location
 {
-    public class LocationData
-    {
-        public List<ResourceSet> ResourceSets { get; set; }
-    }
-
-    public class ResourceSet
-    {
-        public List<Resource> Resources { get; set; }
-    }
-
-    public class Resource
-    {
-        public Point Point { get; set; }
-    }
-
-    public class Point
-    {
-        public List<double> Coordinates { get; set; }
-    }
-
+    /// <summary>
+    /// Defines functionality to retrieve <see cref="GeoCoordinate"/>s from human-friendly location queries.
+    /// </summary>
     public class LocationQuerier
     {
-        private const string LocationFormatString = "http://dev.virtualearth.net/REST/v1/Locations/{0}?o=json&key={1}";
         private readonly string apiKey;
 
+        /// <summary>
+        /// Creates a new <see cref="LocationQuerier"/>.
+        /// </summary>
+        /// <param name="apiKey">The access key to use to retrieve location data. You can get an API key at https://msdn.microsoft.com/en-us/library/ff428642.aspx </param>
         public LocationQuerier(string apiKey)
         {
             this.apiKey = apiKey;
         }
 
+        /// <summary>
+        /// Given a human-friendly location <paramref name="query"/>, such as "NYC", "Boulder, CO", etc, retrieves a machine-friendly <see cref="GeoCoordinate"/>
+        /// </summary>
+        /// <exception cref="InvalidDataException">If the <paramref name="query"/> was unable to be parsed to a <see cref="GeoCoordinate"/></exception>
         public async Task<GeoCoordinate> GetLocationAsync(string query)
         {
             string urlEncodedQuery = HttpUtility.UrlEncode(query);
             using (HttpClient client = new HttpClient())
             {
-                HttpResponseMessage result = 
-                    await client.GetAsync(string.Format(LocationQuerier.LocationFormatString, urlEncodedQuery, this.apiKey)).ConfigureAwait(false);
+                HttpResponseMessage result = await client.GetAsync(
+                    string.Format(Settings.Default.LocationQueryUriFormatString, urlEncodedQuery, this.apiKey)).ConfigureAwait(false);
 
                 string readMessage = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-                LocationData data = JsonConvert.DeserializeObject<LocationData>(readMessage);
+                LocationDataTypes.Resources data = JsonConvert.DeserializeObject<LocationDataTypes.Resources>(readMessage);
                 List<double> coords = data?.ResourceSets?.FirstOrDefault()?.Resources?.FirstOrDefault()?.Point?.Coordinates ?? null;
                 if (coords.Count == 2)
                 {
